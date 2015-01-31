ef72dba153f7391ddada3affa7d3b8ca008e296a
Jonathan Silverman <jsilverman2@gmail.com>
2015-01-30 21:28:12 -0700

    change git link back

--- package.js
+++ package.js
@@ -1,10 +1,10 @@
 Package.describe({
   name: 'jsilvermdx:orangebox',
-  version: '0.0.2',
+  version: '0.0.3',
   // Brief, one-line summary of the package.
   summary: 'A simple, lightweight message box library',
   // URL to the Git repository containing the source code for this package.
-  git: 'git@github.com:jsilverMDX/orangebox.git',
+  git: 'https://github.com/jsilverMDX/orangebox',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
