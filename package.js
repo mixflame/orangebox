8d4a967454fe29c71749daf1b2a756415b50e424
Jonathan Silverman <jsilverman2@gmail.com>
2015-02-05 21:53:28 -0700

    switch back to old method of referencing asset

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'jsilvermdx:orangebox',
-  version: '0.0.14',
+  version: '0.0.15',
   // Brief, one-line summary of the package.
   summary: 'A simple, lightweight message box library',
   // URL to the Git repository containing the source code for this package.
